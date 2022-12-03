b431cf1713d0c9a9f8184402284754055f7dde13
DizzyFlash <DizzyFlash@DESKTOP-1NDV4L2>
2022-12-03 15:41:10 +0200

    Add project files.

---
+++ SEPT/offsets.h
@@ -0,0 +1,12 @@
+#include <iostream>
+
+namespace offsets {
+
+	uintptr_t aLocalPlayer = 0xDE7964;
+	uintptr_t aEntityList = 0x4DFCE74;
+	uintptr_t aViewMatrix = 0x4DEDCA4;
+
+	uintptr_t oTeamNum = 0xF4;
+	uintptr_t oVecOrigin = 0x138;
+	uintptr_t oHealth = 0x100;
+}
